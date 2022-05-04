1353f6c199c1b2b677790803a74502847a57bd5e
litury <litury@icloud.com>
2022-05-04 14:14:12 +0500

    project completed

--- app/js/main.js
+++ app/js/main.js
@@ -1,145 +1,52 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/js/_functions.js":
-/*!******************************!*\
-  !*** ./src/js/_functions.js ***!
-  \******************************/
-/***/ (() => {
-
-
-
-/***/ }),
-
-/***/ "./src/js/_vars.js":
-/*!*************************!*\
-  !*** ./src/js/_vars.js ***!
-  \*************************/
-/***/ (() => {
-
-
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
-(() => {
-"use strict";
 /*!************************!*\
   !*** ./src/js/main.js ***!
   \************************/
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _vars__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./_vars */ "./src/js/_vars.js");
-/* harmony import */ var _vars__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_vars__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _functions__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./_functions */ "./src/js/_functions.js");
-/* harmony import */ var _functions__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_functions__WEBPACK_IMPORTED_MODULE_1__);
-
-
+// Слайдер
 var swiper = new Swiper('.swiper', {
-  // Optional parameters
+  // Прокрутка
   loop: false,
-  // If we need pagination
+  // Точки
   pagination: {
     el: '.swiper-pagination'
   },
-  // Navigation arrows
+  // Навигация
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
-  } // And if we need scrollbar
+  }
+}); // Находим body и rangeText
 
-});
 var range = document.querySelector("#rangeText"),
-    big = document.querySelector("#bigText");
+    big = document.querySelector("#bigText"); // Изменение велечины текста
+
 range.addEventListener("change", function (event) {
   big.style.fontSize = this.value + "px";
-}); // document.body.style.background = "#ccc";
+}); // Смена контраста текста по тегам
 
 var img = document.getElementsByTagName('img');
 var link = document.getElementsByTagName("a");
 var span = document.getElementsByTagName("span");
 var p = document.getElementsByTagName("p");
 var h4 = document.getElementsByTagName('h4');
-var h3 = document.getElementsByTagName('h3');
+var h3 = document.getElementsByTagName('h3'); // Смена контраста бг по тегам по классам
+
 var btn = document.querySelector('.visiBtn');
 var card = document.querySelectorAll('.card');
 var slide = document.querySelectorAll('.feedback__content');
-var elements = document.querySelectorAll('.title');
+var elements = document.querySelectorAll('.title'); // Убираем бг у секций
+
 var heroSection = document.querySelector('.hero__content');
 var findSection = document.querySelector('.find__content');
-var body = document.querySelector('#bigText');
+var body = document.querySelector('#bigText'); // Функция изменения цвета
+
 btn.addEventListener('click', function () {
-  body.classList.toggle('visi');
+  // смена цвета бг 
+  body.classList.toggle('visi'); // убираем бг у секций
+
   heroSection.classList.toggle('bgnone');
-  findSection.classList.toggle('bgnone');
+  findSection.classList.toggle('bgnone'); // проходимся циклом по всем тегам и классам
 
   for (var i = 0; i < img.length; i++) {
     img[i].classList.toggle('imgnone');
@@ -176,9 +83,19 @@ btn.addEventListener('click', function () {
   for (var i = 0; i < elements.length; i++) {
     elements[i].classList.toggle('test');
   }
-});
-})();
+}); // Липкая шапка
 
+var header = document.querySelector('.header__top'); // Событие скрола
+
+document.addEventListener('scroll', headerFixed); // Функция добавления класса при скроле
+
+function headerFixed() {
+  if (window.scrollY > 30) {
+    header.classList.add('header__top_bg');
+  } else {
+    header.classList.remove('header__top_bg');
+  }
+}
 /******/ })()
 ;
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,53 @@
-import vars from './_vars';
-import './_functions';
-
+// Слайдер
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-
+  // Прокрутка
   loop: false,
-
-  // If we need pagination
+  // Точки
   pagination: {
     el: '.swiper-pagination',
   },
-
-  // Navigation arrows
+  // Навигация
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
 });
 
+// Находим body и rangeText
 let range = document.querySelector("#rangeText"),
   big = document.querySelector("#bigText");
-
+// Изменение велечины текста
 range.addEventListener("change", function (event) {
   big.style.fontSize = this.value + "px";
 });
 
-// document.body.style.background = "#ccc";
-
-
+// Смена контраста текста по тегам
 let img = document.getElementsByTagName('img');
 let link = document.getElementsByTagName("a");
 let span = document.getElementsByTagName("span");
 let p = document.getElementsByTagName("p");
 let h4 = document.getElementsByTagName('h4');
 let h3 = document.getElementsByTagName('h3');
-
+// Смена контраста бг по тегам по классам
 let btn = document.querySelector('.visiBtn');
 let card = document.querySelectorAll('.card');
 let slide = document.querySelectorAll('.feedback__content');
 let elements = document.querySelectorAll('.title');
-
+// Убираем бг у секций
 let heroSection = document.querySelector('.hero__content');
 let findSection = document.querySelector('.find__content');
 let body = document.querySelector('#bigText');
 
-
-
+// Функция изменения цвета
 btn.addEventListener('click', function () {
-
+  // смена цвета бг 
   body.classList.toggle('visi');
 
-  
+  // убираем бг у секций
   heroSection.classList.toggle('bgnone');
   findSection.classList.toggle('bgnone');
 
-
+  // проходимся циклом по всем тегам и классам
   for (var i = 0; i < img.length; i++) {
     img[i].classList.toggle('imgnone');
   }
@@ -93,3 +84,17 @@ btn.addEventListener('click', function () {
     elements[i].classList.toggle('test');
   }
 })
+
+// Липкая шапка
+const header = document.querySelector('.header__top')
+// Событие скрола
+document.addEventListener('scroll', headerFixed);
+
+// Функция добавления класса при скроле
+function headerFixed() {
+  if (window.scrollY > 30) {
+    header.classList.add('header__top_bg');
+  } else {
+    header.classList.remove('header__top_bg');
+  }
+}
